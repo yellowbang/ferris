bf7db271e50aac1917debd9627ae2b48e95b7711
bonhuan <bonhuan@cisco.com>
2018-12-11 16:38:06 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BarChart} from 'react-d3-components';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render () {
+
+		var data = [
+			{
+				label: 'somethingA',
+				values: [{x: 'SomethingA', y: 10}, {x: 'SomethingB', y: 4}, {x: 'SomethingC', y: 3}]
+			}
+		];
+
+		const n = 10;
+		let pics = [];
+
+		for (let i = 0; i < n; i++) {
+			let deg = 360 / n * i;
+			let picDeg = -deg;
+			pics.push(
+				<div className="orbit-container" key={"pic" + i} style={{transform: "rotate(" + deg + "deg)"}}>
+					<div className="orbit">
+						<div className="pic-container">
+							<img src={logo} alt="logo" style={{transform: "rotate(" + picDeg + "deg)"}}/>
+						</div>
+					</div>
+				</div>
+			)
+		}
+		return (
+			<div className="app">
+				<BarChart data={data} width={400} height={400} margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
+				<div className="pics-container">
+					{pics}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
