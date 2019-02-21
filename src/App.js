74ac484b5535fa9e9a9c0ca1853917909ce9c425
bonhuan <bonhuan@cisco.com>
2019-02-21 15:35:04 -0800

    demo with mock data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
 import React, {Component} from 'react';
 import './App.scss';
+import ReactTable from "react-table";
+import {LineChart} from "react-d3-components";
+import 'react-table/react-table.css';
+import DATA from "./Data";
+
+const COLUMNS = [
+    {
+        Header: 'Port',
+        accessor: 'portId',
+    }, {
+        id: 'temp',
+        Header: 'Temperature',
+        accessor: d => d.temps[d.temps.length - 1].y
+    }, {
+        Header: 'CPU',
+        accessor: 'cpu',
+    }, {
+        Header: 'Other Props',
+        accessor: 'other'
+    }
+];
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: [{
+                label: '',
+                values: [{x: 0, y: 0}]
+            }]
+        };
+    }
+
+    populateChart = (rowInfo) => {
+        let chartData = [{
+            label: rowInfo.portId,
+            values: rowInfo.temps,
+        }];
+        this.setState({
+            chartData
+        })
+    };
+
     render () {
+        const me = this;
 
         return (
             <div className="app">
                 <div className="session-container left-container">
-                    aa
+                    <ReactTable
+                        data={DATA}
+                        columns={COLUMNS}
+                        getTdProps={(state, rowInfo) => {
+                            return {
+                                onClick: () => {
+                                    me.populateChart(rowInfo.original);
+                                }
+                            };
+                        }}
+                    />
                 </div>
                 <div className="session-container right-container">
-                    bb
+                    <LineChart
+                        data={this.state.chartData}
+                        width={600}
+                        height={600}
+                        xAxis={{label: "time"}}
+                        yAxis={{label: "temperature"}}
+                        margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
                 </div>
             </div>
         );
