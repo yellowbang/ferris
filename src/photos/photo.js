65a1af81b32ccbecfa8ae62bb9a8557136e75c2a
bonhuan <bonhuan@cisco.com>
2019-02-25 23:53:14 -0800

    add music and photo

---
+++ src/photos/photo.js
@@ -0,0 +1,18 @@
+import wenquan from './wenquan.jpg';
+import jiehun from './jiehun.jpeg';
+import kayak from './kayak.jpeg';
+
+export default [
+    {
+        src: wenquan,
+        descr: '第一次泡温泉',
+    },
+    {
+        src: jiehun,
+        descr: '第一次泡温泉2',
+    },
+    {
+        src: kayak,
+        descr: '第一次泡温泉3',
+    },
+]
