7cb989fe023c1e4944db6673156b408c8a06dafe
Bon Huang <bon@redmarlin.ai>
2020-06-30 01:44:00 -0700

    new init and add birthday2020703

---
+++ src/views/Birthday/Birthday20200703.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import './Birthday20200703.scss';
+import letter from './letter.jpg';
+import Audio from './Audio';
+
+class Birthday20200703 extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div className="Birthday20200703">
+        <div className="texts-container">
+          <div className="texts">
+            Hello 最卑鄙的小肥人，
+
+            你说这个世界上怎么会有又傻又笨又矮又胖又得意又卑鄙又搞屎棍又勇敢又善良又焖姜又会安慰自己的人存在啊。小时候看别人结婚，看到新娘是个胖妞的时候，心里总会想，为什么新郎不取一个美美的新娘呢。现在，我明白了。。哪些奇特的脸其实更美。。是一种令人看到之后就会觉得开心，觉得笑到心里的美。。
+          </div>
+        </div>
+        <div className="texts-container">
+          <div className="texts">
+            今晚又系你不在家的一晚。虽然眼睛见不到你，但脑海中总浮现出你的样子，各种各样的表情。其中有一个表情系我特别中意的。就是你自得其乐而满足的样子。系咪唔知我讲紧咩表情呢。当你吃到你中意吃的野，或者睇住喜欢的电视节目时就会表现出来的表情。那是一种小朋友得到自己喜欢的玩具时的幸福感。虽然我并不强大，但希望可以守护着你的这份幸福感。
+          </div>
+        </div>
+        <div className="texts-container">
+          <div className="texts">
+            多谢你陪我做各种各样傻更更的事。虽然我遇见不少女生，但只有你会懂我的的傻，而且还会陪我一起傻。好开心有这么一个你，30多岁的我地，可以一起以3岁小朋友甘的模式相处。平时和你一起打斗，一起互相整蛊对方，互相吓吓对方，现在回忆起来，都是会笑的场景。希望在以后的日子，和你有更多的哭笑不得的情景。对了，可能不止有我和你哟。。
+          </div>
+        </div>
+        <div className="texts-container">
+          <div className="texts">
+            祝你生日快乐，又大一岁了，记得不要总欺负老公啊。 😏。
+            <br/>
+            <br/>
+            （PS: 字不是特别的漂亮，但已经是最有诚意的字体了）
+            <br/>
+            <br/>
+            我是爱你与你爱的老公
+          </div>
+        </div>
+        <Audio/>
+      </div>
+    );
+  }
+}
+
+export default Birthday20200703;
