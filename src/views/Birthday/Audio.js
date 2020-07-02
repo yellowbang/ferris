871f735f7429543beb6850b3ddfd4152f539ab42
Bon Huang <bon@redmarlin.ai>
2020-07-01 20:07:14 -0700

    minor change

--- src/views/Birthday/Audio.js
+++ src/views/Birthday/Audio.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Birthday20200703.scss';
 import kannikanwo from '../../sound/kannikanwo.mp3';
 
 class Audio extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-
-        return (
-            <div className="audio-container">
-                <audio src={kannikanwo} controls autoPlay onEnded={this.nextSong}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="audio-container">
+        <audio src={kannikanwo} controls autoPlay onEnded={this.nextSong}/>
+      </div>
+    );
+  }
 }
 
 export default Audio;
